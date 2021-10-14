3132dceed0d72e4770ed9d42f9f44c8bd317efd9
Ricardo Moreira <rgusto@gmail.com>
2021-10-14 17:05:30 -0300

    Configuração de banco

--- src/main/java/br/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/br/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.algaworks.algafoodapi;
+package br.com.algaworks.algafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
